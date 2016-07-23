0b08e39a0d8062ec8e5795e9cb6fa7cf47fd5286
mbolanosc <mrybc96@gmail.com>
2016-07-23 16:21:27 -0600

    redireccionar

---
+++ window.js
@@ -0,0 +1,10 @@
+
+ var eBtnWindow = document.querySelector('#btn-Window');
+
+ eBtnWindow.addEventListener('click', redireccionar);
+
+ function redireccionar(){
+
+window.location=('game.html');
+
+}
